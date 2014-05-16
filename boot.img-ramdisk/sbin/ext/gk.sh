05a0cf23cddfe6ac66a1f560858bef38e30ffde1
Willi Ye <williye97@gmail.com>
2014-05-16 22:47:57 +0200

    update kernel
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- boot.img-ramdisk/sbin/ext/gk.sh
+++ boot.img-ramdisk/sbin/ext/gk.sh
@@ -10,6 +10,8 @@ ln -s /lib/modules/* /system/lib/modules/
 echo 2 > /sys/devices/system/cpu/sched_mc_power_savings
 echo 1728000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 echo 162000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1728000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo 162000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 echo 1 > /sys/kernel/dyn_fsync/Dyn_fsync_active
 
 ln -s /res/synapse/uci /sbin/uci
